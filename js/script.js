8a7914b0a7a275694bdfe8ec657387012edace68
Francisco Afonso <afonso.webdev@gmail.com>
2025-05-02 22:47:22 +0100

    Começando os primeiro passos em javascript

--- js/script.js
+++ js/script.js
@@ -6,3 +6,11 @@ const menu = document.getElementById('nav-menu');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+
+// Close the menu when a link is clicked
+const menuLinks = document.querySelectorAll('.nav-link');
+menuLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        menu.classList.remove('active');
+    });
+});
