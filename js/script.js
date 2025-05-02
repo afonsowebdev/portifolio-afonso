d990c70116d64ac368252ea6d8156c898d7ad31a
Francisco Afonso <afonso.webdev@gmail.com>
2025-05-02 22:56:49 +0100

    Alterações feitas com sucesso

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 /* Menu Targge */
 
+/* Para podermos criar uma interação para poder abrir o menu suspenso e necessário buscar a class .menu-toggle que é o nosso botão de menu  */
+
 const toggle = document.getElementById('menu-toggle');
 const menu = document.getElementById('nav-menu');
 
@@ -7,7 +9,9 @@ toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
 
-// Close the menu when a link is clicked
+// Close the menu when a link is 
+
+/* Para podermos criar uma interação para poder fechar o menu suspenso e necessário buscar a class .nav-link que é o conjunto dos nossos links  */
 const menuLinks = document.querySelectorAll('.nav-link');
 menuLinks.forEach(link => {
     link.addEventListener('click', () => {
